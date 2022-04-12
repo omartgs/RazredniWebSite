9827fbedfdd1c14388eff35de861164c91ee199e
omartgs <omar.jeina@treca-gimnazija.edu.ba>
2022-04-12 10:13:04 +0200

    v1.0

---
+++ js/home.js
@@ -0,0 +1,3 @@
+function homeFunction(){
+    window.location = "home.html";
+}
---
+++ js/scrollToTop.js
@@ -0,0 +1,7 @@
+function scrollToTop(){
+    var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+    if (currentScroll > 0) {
+         window.requestAnimationFrame(scrollToTop);
+         window.scrollTo (0,currentScroll - (currentScroll/15));
+    }
+};
